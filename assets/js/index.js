c4a8d267d113b90841e064ee7e5691ebc856701a
Ben Leong <bleong@ucdavis.edu>
2014-09-01 21:56:56 -0700

    added navbar and button on click add random stuff

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,15 +2,25 @@
 
 (function(){
 
-	var app = angular.module('benri', []);
+  var app = angular.module('benri', []);
 
-	app.controller('bodyCtrl', ['$scope', function bodyController($scope){
-	
-		$scope.stuff = 'Pizza';
+  app.controller('bodyCtrl', ['$scope', function bodyController($scope){
+  
+    $scope.stuff = ['Pizza','Hotpocket'];
 
-		$scope.stuff2 = 'Hotpocket';
+    $scope.add_stuff = function() {
+      var new_stuff = '';
+      var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      var possible = "abcdefghijklmnopqrstuvwxyz";
+      new_stuff += caps.charAt(Math.floor(Math.random() * caps.length));
+      for (var i = 1; i < 6; i++) {
+        new_stuff += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
 
-	}])
+      $scope.stuff.push(new_stuff);
+    };
+
+  }])
 
 })();
 
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,15 +1,32 @@
 'use strict';
 
-describe('bodyCtrl', function(){
+describe('bodyCtrl', function() {
 
-  beforeEach(module('benri'));
+  var $rootScope, $scope, $controller;
 
-  it('should create "stuff" model with empty string, and "stuff2" model with "stuff"', inject(function($controller) {
-    var scope = {},
-      ctrl = $controller('bodyCtrl', {$scope:scope});
+  beforeEach(function () {
+    module('benri');
+  });
 
-    expect(scope.stuff).toBe('Pizza');
-    expect(scope.stuff2).toBe('Hotpocket');
+  beforeEach(inject(function(_$rootScope_, _$controller_){
+    $rootScope = _$rootScope_;
+    $scope = $rootScope.$new();
+    $controller = _$controller_;
+
+    $controller('bodyCtrl', {'$rootScope' : $rootScope, '$scope': $scope});
   }));
+
+  it('should create "stuff" model with empty string, and "stuff2" model with "stuff"', function() {
+
+    expect($scope.stuff.length).toBe(2);
+    expect($scope.stuff[0]).toBe('Pizza');
+
+  });
+
+  it('should increase length of stuff by 1', function(){
+
+    //expect(finalSize).toBe(initialSize + 1);
+
+  });
 
 });
